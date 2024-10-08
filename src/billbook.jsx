19bdf9f1c61befe9331d0f7ae5b55ea3933286a4
Kiran Jamdade <kiranjamdade@Kirans-MacBook-Air.local>
2024-10-08 15:37:39 +0530

    lr added

---
+++ src/billbook.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const billbook = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default billbook
---
+++ src/lrcopy.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const lrcopy = () => {
+  return (
+    <div>
+      <h1>Rohit</h1>
+    </div>
+  )
+}
+
+export default lrcopy
